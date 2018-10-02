ee97641b1f7482118f0d6534ec8a1a3456461f5e
AliceVandenberghe <43027951+AliceVandenberghe@users.noreply.github.com>
2018-10-02 20:15:30 +0200

    Avogadro afgewerkt

---
+++ 04 - Variabelen/Avogadro.py
@@ -0,0 +1,16 @@
+Na = 6.020 * (10 ** 23)
+Ms = 32.06
+
+# invoer
+ns = float(input('Aantal deeltjes = '))
+
+# bewerkingen
+Ns = ns * Na
+ms = Ms * ns
+
+# uitvoer
+resultaat1 = str(ms)
+resultaat2 = str(Ns)
+
+print(resultaat1)
+print(resultaat2)
