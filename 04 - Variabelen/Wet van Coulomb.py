2092f7939772324596a796112f977b7baa7795ce
AliceVandenberghe <43027951+AliceVandenberghe@users.noreply.github.com>
2018-10-02 19:55:40 +0200

    Wet van Coulomb afgewerkt

--- 04 - Variabelen/Wet van Coulomb.py
+++ 04 - Variabelen/Wet van Coulomb.py
@@ -1,11 +1,15 @@
 k = 8.99 * (10 ** 9)
-q1 = 2 * (10 ** (-6))
+q1 = 2.0 * (10 ** (-6))
 q2 = 1.0 * (10 ** (-6))
 
 #de invoer
 r = float(input('straal: '))
+r *= 10 ** -2
 
-resultaat = 'Fc'
-resultaat = (k * q1 * q2) / (r ** 2)
+#berekening
+fc = k *(( q1 * q2) / (r ** 2))
+
 #uitvoer
+resultaat = fc
+
 print(resultaat)
