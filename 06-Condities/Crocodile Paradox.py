584b224560b84318dc241795e6c5ed28d197c4a0
AliceVandenberghe <43027951+AliceVandenberghe@users.noreply.github.com>
2018-10-21 20:49:18 +0200

    Crocodile Paradox afgewerkt

--- 06-Condities/Crocodile Paradox.py
+++ 06-Condities/Crocodile Paradox.py
@@ -1,3 +1,7 @@
 antwoord = input('wie heeft stalen zenuwen?')
 
-if antwoord =='vader':
+if antwoord =='vader': print('Krokodil geeft kind terug')
+
+elif antwoord =='krokodil': print('Krokodil eet kind op')
+
+else: print('Moe van het denken')
