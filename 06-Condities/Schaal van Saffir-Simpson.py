28d3105a71b4af7ddd43fb6220792d8cf1f806ec
AliceVandenberghe <43027951+AliceVandenberghe@users.noreply.github.com>
2018-10-23 19:39:41 +0200

    Schaal van Saffir-Simpson afgewerkt

---
+++ 06-Condities/Schaal van Saffir-Simpson.py
@@ -0,0 +1,19 @@
+antwoord = int(input('Wat is de windsnelheid? '))
+
+if antwoord < 119:
+    print('geen orkaan')
+
+elif antwoord >= 210 and antwoord <= 249:
+    print('categorie 4')
+
+elif antwoord >= 178 and antwoord <= 209:
+    print('categorie 3')
+
+elif antwoord >= 154 and antwoord <= 177:
+    print('categorie 2')
+
+elif antwoord >= 199 and antwoord <= 153:
+    print('categorie 1')
+
+else:
+    print('categorie 5')
