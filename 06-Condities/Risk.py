3b38ed039782d1a6ce48a7e1a44d34ef4ea832c9
AliceVandenberghe <43027951+AliceVandenberghe@users.noreply.github.com>
2018-10-23 22:00:38 +0200

    Risk afgewerkt

---
+++ 06-Condities/Risk.py
@@ -0,0 +1,22 @@
+verliezer = 'aanvaller verliest 1 leger, verdediger verliest 1 leger'
+
+#input
+a = int(input('de eerste dobbelsteen van de aanvaller: '))
+b = int(input('de tweede dobbelsteen van de aanvaller: '))
+c = int(input('de derde dobbelsteen van de aanvaller: '))
+d = int(input('de eerste dobbelsteen van de verdediger: '))
+e = int(input('de tweede dobbelsteen van de verdediger: '))
+
+getal_2 = (a + b + c) - max(a, b, c)- min(a, b, c)
+
+#berekeningen
+if max(a, b, c) > max(d, e):
+   if getal_2 > min(d, e):
+        print('aanvaller verliest 0 legers, verdediger verliest 2 legers')
+    elif getal_2 <= min(d, e):
+         print(verliezer)
+elif max(a, b, c) <= max(d, e):
+    if getal_2 <= min(d, e):
+        print('aanvaller verliest 2 legers, verdediger verliest 0 legers')
+    elif getal_2 > min(d, e):
+        print(verliezer)
