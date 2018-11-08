307b8706e5e3d7f5985492e789e30b5abf79d482
AliceVandenberghe <43027951+AliceVandenberghe@users.noreply.github.com>
2018-11-08 10:01:43 +0100

    omkeren afgewerkt

---
+++ 07a-Iteraties-For-lus/Omkeren.py
@@ -0,0 +1,8 @@
+woord = input('woord: ')
+omgekeerd_woord = ''
+
+for letter in woord:
+    omgekeerd_woord = letter + omgekeerd_woord
+
+print(omgekeerd_woord)
+
---
+++ 07a-Iteraties-For-lus/veelvouden.py
@@ -0,0 +1,7 @@
+r = (int(input('getal: ')))
+som_veelvouden = 0
+
+#overloop alle veelvouden van r
+for i in range(...,...,...):
+
+print(som_veelvouden)
