13a30302b1c91880cff15518b046f04adcd79d66
AliceVandenberghe <43027951+AliceVandenberghe@users.noreply.github.com>
2018-11-13 20:27:47 +0100

    Het grootste gemiddelde afgewerkt

---
+++ 07a-Iteraties-For-lus/Het grootste en het gemiddelde.py
@@ -0,0 +1,17 @@
+# invoer
+aantal_getallen = int(input('aantal getallen: '))
+max = 0
+som = 0
+
+for i in range(aantal_getallen):
+    getal = int(input('Getal: '))
+    if i == 0:
+        max = getal
+    elif getal > max:
+        max = getal
+    som += getal
+
+gemiddelde = round((som / aantal_getallen), 2)
+
+#uitvoer
+print('{} {:d} {} {:.2f}'.format('Het grootste getal is', max, 'en het gemiddelde is', gemiddelde))
